780281742d1ee2f0d3a7110c8945bb119ba3c3a4
Raymond shi <rs5955@nyu.edu>
2021-01-30 02:07:31 -0500

    Add tts

--- app/src/main/java/com/example/ttsbutton/MainActivity.kt
+++ app/src/main/java/com/example/ttsbutton/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.ttsbutton
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
+import android.widget.Button
+import android.widget.EditText
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
+    lateinit var button : Button
+    lateinit var text : EditText
+    lateinit var tts : TextToSpeech
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button = findViewById(R.id.button)
+        text = findViewById(R.id.editTextTextMultiLine)
+
+//        button.setOnClickListener { text.setText("hello") }
+
+        tts = TextToSpeech(this, this)
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+    override fun onInit(status: Int) {
+        button.setOnClickListener { speak() }
+    }
+
+    private fun speak() {
+        tts.speak(text.text.toString(), TextToSpeech.QUEUE_FLUSH,null,"")
     }
 }
